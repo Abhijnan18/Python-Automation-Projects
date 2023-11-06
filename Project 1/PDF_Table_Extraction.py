a5d3f00d244c7bf30c6f4e505ef8489b99d39b9a
Abhijnan S <114734468+Abhijnan18@users.noreply.github.com>
2023-11-06 10:08:44 +0530

    Commit changes made to PDF_Table_Extraction.py

--- Project 1/PDF_Table_Extraction.py
+++ Project 1/PDF_Table_Extraction.py
@@ -1,13 +1,25 @@
 '''
 📜 PDF Table Extraction and CSV Storage 📊
 '''
-import camelot
+import tabula
 
-# Read tables from the PDF file
-tables = camelot.read_pdf('points_table.pdf', pages='1')
+# PDF file to extract tables from
+pdf_file = "points_table.pdf"
 
-# Export the first table to a CSV file
-if tables:
-    tables[0].to_csv('points_table.csv')
-else:
+# Extract tables from the PDF
+tables = tabula.read_pdf(pdf_file, pages='all')
+
+# Check if any tables were found in the PDF
+if len(tables) == 0:
     print("No tables found in the PDF.")
+else:
+    # Export the first table to a CSV file
+    first_table = tables[0]
+    first_table.to_csv("output_table.csv", index=False)
+
+    print("First table extracted and exported to 'output_table.csv'.")
+
+
+
+
+
