e2603868ec957500776f867d021911c0d88b9552
Abhijnan S <114734468+Abhijnan18@users.noreply.github.com>
2023-11-07 10:19:51 +0530

    Added the 1.news-extract-data.py and output .csv file

---
+++ 2.Automate The News/1.news-extract-data.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import pandas as pd
+
+website = "https://www.thesun.co.uk/sport/football/"
+path = "/Users/abhijnans/Downloads/chromedriver-mac-arm64"
+
+service = Service(excutable_path=path)
+driver = webdriver.Chrome(service=service)
+driver.get(website)
+
+containers = driver.find_elements(by="xpath", value='//div[@class = "teaser__copy-container"]')
+
+titles = []
+subtitles = []
+links = []
+
+for container in containers:
+    title = container.find_element(by="xpath", value='./a').text
+    subtiltle = container.find_element(by="xpath", value='./a/h3').text 
+    link = container.find_element(by="xpath",value='./a' ).get_attribute("href")
+    titles.append(title)
+    subtitles.append(subtiltle)
+    links.append(link)
+
+my_dict = {'title' : titles, 'subtiltle': subtitles, 'link': links }
+
+df_headlines = pd.DataFrame(my_dict)
+df_headlines.to_csv('headline.csv')
+
+driver.quit()
