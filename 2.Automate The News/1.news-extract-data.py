67c3e79b5334a1100deb15c2a6dbbdff8e3f34ae
Abhijnan S <114734468+Abhijnan18@users.noreply.github.com>
2023-11-07 10:21:22 +0530

    Corrected the indentaition of 1.news-extract-data.py

--- 2.Automate The News/1.news-extract-data.py
+++ 2.Automate The News/1.news-extract-data.py
@@ -9,7 +9,8 @@ service = Service(excutable_path=path)
 driver = webdriver.Chrome(service=service)
 driver.get(website)
 
-containers = driver.find_elements(by="xpath", value='//div[@class = "teaser__copy-container"]')
+containers = driver.find_elements(
+    by="xpath", value='//div[@class = "teaser__copy-container"]')
 
 titles = []
 subtitles = []
@@ -17,13 +18,14 @@ links = []
 
 for container in containers:
     title = container.find_element(by="xpath", value='./a').text
-    subtiltle = container.find_element(by="xpath", value='./a/h3').text 
-    link = container.find_element(by="xpath",value='./a' ).get_attribute("href")
+    subtiltle = container.find_element(by="xpath", value='./a/h3').text
+    link = container.find_element(
+        by="xpath", value='./a').get_attribute("href")
     titles.append(title)
     subtitles.append(subtiltle)
     links.append(link)
 
-my_dict = {'title' : titles, 'subtiltle': subtitles, 'link': links }
+my_dict = {'title': titles, 'subtiltle': subtitles, 'link': links}
 
 df_headlines = pd.DataFrame(my_dict)
 df_headlines.to_csv('headline.csv')
