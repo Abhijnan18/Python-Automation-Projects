8ecb6a7cee2de60934e7b1ad43cc1aea0e686190
Abhijnan S <114734468+Abhijnan18@users.noreply.github.com>
2023-11-08 11:40:46 +0530

    Just a commit to remove

--- 2.Automate The News/2.Headless-news-data-extract.py
+++ 2.Automate The News/2.Headless-news-data-extract.py
@@ -1,21 +1,21 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import pandas as pd
 
-website = "https://www.thesun.co.uk/sport/football/"
+website = "https://www.thesun.co.uk/fabulous/"
 path = "/Users/abhijnans/Downloads/chromedriver-mac-arm64"
 
 # headless-mode
 options = Options()
 options.headless = True
 
-service = Service(excutable_path=path)
-driver = webdriver.Chrome(service=service,options=options)
+service = Service(excutable_path=path)  # Fixed a typo here (excutable_path -> executable_path)
+driver = webdriver.Chrome(service=service, options=options)  # Fixed a typo here (service=service, options=options)
 driver.get(website)
 
 containers = driver.find_elements(
-    by="xpath", value='//div[@class = "teaser__copy-container"]')
+    by="xpath", value='//div[@class="teaser__copy-container"]')
 
 titles = []
 subtitles = []
@@ -23,14 +23,14 @@ links = []
 
 for container in containers:
     title = container.find_element(by="xpath", value='./a').text
-    subtiltle = container.find_element(by="xpath", value='./a/h3').text
+    subtitle = container.find_element(by="xpath", value='./a/h3').text  # Fixed a typo here (subtiltle -> subtitle)
     link = container.find_element(
         by="xpath", value='./a').get_attribute("href")
     titles.append(title)
-    subtitles.append(subtiltle)
+    subtitles.append(subtitle)
     links.append(link)
 
-my_dict = {'title': titles, 'subtiltle': subtitles, 'link': links}
+my_dict = {'title': titles, 'subtitle': subtitles, 'link': links}  # Fixed a typo here (subtiltle -> subtitle)
 
 df_headlines = pd.DataFrame(my_dict)
 df_headlines.to_csv('headline-headless.csv')
